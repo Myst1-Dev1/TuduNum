22929ee0a7f5917e3ffb92a4cb96607a6388963c
João Victor <mtheusmss@gmail.com>
2026-06-04 01:04:51 -0300

    ajuste no environment

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://lab.mystdev.com.br/api/tudu-num-api', // production api
+  apiUrl: 'https://lab.mystdev.com.br/api/tudu-num-api', // production api
   vapidPublicKey: 'BEl62vj6s54B...your-production-vapid-key-here'
 };
