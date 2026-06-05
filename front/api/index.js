87bf505b27894330bd1e9a0d8a5e8341cff71225
João Victor <mtheusmss@gmail.com>
2026-06-04 21:33:13 -0300

    ajuste

--- front/api/index.js
+++ front/api/index.js
@@ -1,1 +1,12 @@
-export default import('../dist/front/server/server.mjs').then(module => module.app);
+export default async (req, res) => {
+  try {
+    // Importa o módulo do servidor gerado pelo Angular 19
+    const { app } = await import('../dist/front/server/server.mjs');
+    
+    // Passa a requisição e a resposta para o Express do Angular tratar
+    return app(req, res);
+  } catch (error) {
+    console.error('Erro ao carregar o servidor do Angular:', error);
+    res.status(500).send('Erro Interno do Servidor no Vercel');
+  }
+};
